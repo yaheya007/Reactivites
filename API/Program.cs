d4e38432213fbc227a009045d33e711c98243046
yaheya007 <115330652+yaheya007@users.noreply.github.com>
2023-11-09 13:35:35 -0500

    End Of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using API.Extensions;
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,18 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt =>{
-    opt.AddPolicy("CorsPolicy", policy =>{
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://Localhost:3000");
-    });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+
 
 var app = builder.Build();
 
@@ -46,8 +40,8 @@ try
 }
 catch (Exception ex)
 {
-    
-   var logger = services.GetRequiredService<ILogger<Program>>();
+
+    var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occured during migration");
 }
 
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,37 @@
+using MediatR;
+using Persistence;
+using AutoMapper;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Domain.Activity Activity { get; set; }
+        }
+
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                _mapper.Map(request.Activity, activity);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value; // Equivalent to Nothing
+            }
+        }
+    }
+}
